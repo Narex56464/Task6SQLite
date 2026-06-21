b507c862a350f8f006237ce67d6880a8b1129de0
Narex56464 <n3gativen@yandex.ru>
2026-06-21 20:07:59 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Task6SQLite.Model;
+
+namespace Task6SQLite
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            DatabaseFacade facade = new DatabaseFacade(new ModelContext());
+            facade.EnsureCreated();
+        }
+    }
+
+}
---
+++ Model/ModelContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task6SQLite.Model
+{
+    public class ModelContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = Test.db");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
